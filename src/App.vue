f33452defcfee7542e484d35f186f94353d28ec1
Tobira <tobirawork@gmail.com>
2017-07-14 11:10:12 +0300

    navigation and toolbar almost done

--- src/App.vue
+++ src/App.vue
@@ -1,111 +1,38 @@
-<template>
-  <v-app light>
-    <v-navigation-drawer
-      persistent
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      v-model="drawer"
-      enable-resize-watcher
-    >
-      <v-list>
-        <v-list-tile
-          value="true"
-          v-for="(item, i) in items"
-          :key="i"
-        >
-          <v-list-tile-action>
-            <v-icon light v-html="item.icon"></v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title v-text="item.title"></v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-toolbar fixed>
-      <v-toolbar-side-icon @click.native.stop="drawer = !drawer" light></v-toolbar-side-icon>
-      <v-btn
-        icon
-        light
-        @click.native.stop="miniVariant = !miniVariant"
-      >
-        <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        light
-        @click.native.stop="clipped = !clipped"
-      >
-        <v-icon>web</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        light
-        @click.native.stop="fixed = !fixed"
-      >
-        <v-icon>remove</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title"></v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        icon
-        light
-        @click.native.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>menu</v-icon>
-      </v-btn>
-    </v-toolbar>
-    <main>
-      <v-container fluid>
-        <v-slide-y-transition mode="out-in">
-          <v-layout column align-center>
-            <img src="/static/v.png" alt="Vuetify.js" class="mb-5">
-            <blockquote>
-              &#8220;First, solve the problem. Then, write the code.&#8221;
-              <footer>
-                <small>
-                  <em>&mdash;John Johnson</em>
-                </small>
-              </footer>
-            </blockquote>
-          </v-layout>
-        </v-slide-y-transition>
-      </v-container>
-    </main>
-    <v-navigation-drawer
-      temporary
-      :right="right"
-      v-model="rightDrawer"
-    >
-      <v-list>
-        <v-list-tile @click.native="right = !right">
-          <v-list-tile-action>
-            <v-icon light>compare_arrows</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer :fixed="fixed">
-      <span>&copy; 2017</span>
-    </v-footer>
-  </v-app>
+<template lang="pug">
+  v-app
+    //- navigation-drawer content
+    v-navigation-drawer( v-model="sideNav" temporary class="green lighten-3")
+      v-list( class="green lighten-3" )
+        v-list-tile( v-for="(item, i) in nav" :key="i" )
+          v-list-tile-action
+            v-icon {{ item.icon }}
+          v-list-tile-content {{ item.text }}
+         
+
+    //- toolbar content
+    v-toolbar( dark class="primary" )
+      v-toolbar-side-icon( @click.native.stop="sideNav = !sideNav" class="hidden-sm-and-up" )
+      v-toolbar-title( class="text-xs-center" )="DevMeetups"
+      v-spacer
+      v-toolbar-items( class="hidden-xs-only" v-for="(item, i) in nav" :key="i" )
+        v-btn(flat)
+          v-icon( dark ) {{ item.icon }}
+          | {{ item.text }}
+    //- main content
+    main 
 </template>
+
 
 <script>
   export default {
     data () {
       return {
-        clipped: false,
-        drawer: true,
-        fixed: false,
-        items: [
-          { icon: 'bubble_chart', title: 'Inspire' }
-        ],
-        miniVariant: false,
-        right: true,
-        rightDrawer: false,
-        title: 'Vuetify.js'
+        sideNav: false,
+        nav: [
+          { icon: 'supervisor_account', text: 'View Meetups' },
+          { icon: 'note_add', text: 'New Meetup' },
+          { icon: 'account_circle', text: 'Profile' },
+        ]
       }
     }
   }
